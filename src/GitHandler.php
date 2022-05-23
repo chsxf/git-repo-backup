e6870817f9346895d0ad032dab81bc4824f7625e
Christophe SAUVEUR <chsxf.pro@gmail.com>
2022-05-23 14:13:45 +0200

    ✨ GitHandler - Check git availability

---
+++ src/GitHandler.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace chsxf;
+
+class GitHandler
+{
+    public static function checkGitAvailability(): bool
+    {
+        Console::log('Looking for git...');
+
+        $result = exec('git --version');
+        if ($result !== false && preg_match('/^git version \d+\.\d+\.\d+/', $result)) {
+            Console::success('%s', $result);
+        } else {
+            Console::error('It seems git is not installed on the system or not accessible in the current environment');
+            return false;
+        }
+
+        Console::log('Looking for git-lfs...');
+        $result = exec('git lfs version');
+        if ($result !== false && preg_match('/^git-lfs\/\d+\.\d+\.\d+/', $result)) {
+            Console::success('%s', $result);
+        } else {
+            Console::error('It seems git-lfs is not installed on the system or not accessible in the current environement');
+            return false;
+        }
+
+        return true;
+    }
+}
